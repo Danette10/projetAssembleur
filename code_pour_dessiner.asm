0948a3b0282941f17791db7cb83f12f964874148
Dan Sebag <dan.sebag1007@gmail.com>
2023-02-04 11:55:40 +0100

    add function to generate random number and create rand coords

--- code_pour_dessiner.asm
+++ code_pour_dessiner.asm
@@ -46,6 +46,8 @@ gc:		resq	1
 
 section .data
 
+reponse1: db "Coordonnées: (%d,%d), (%d,%d)",10,0
+
 event:		times	24 dq 0
 
 x1:	dd	0
@@ -54,12 +56,81 @@ y1:	dd	0
 y2:	dd	0
 
 section .text
+
+
+;##################################################
+;########### FONCTION POUR GENERER UN #############
+;############## NOMBRE ALEATOIRE ##################
+;##################################################
+
+; la fonction pour générer des nombres aléatoires entiers
+; prend en entrée un pointeur vers un espace de stockage pour le résultat
+; et retourne ce dernier dans EAX
+generer_nombre_aleatoire:
+push rbp
+mov rax,0
+rdrand ax
+jnc lbl_relancer
+mov [rdi],ax
+jmp lbl_fin
+lbl_relancer:
+call generer_nombre_aleatoire
+lbl_fin:
+pop rbp
+ret
+
+
 	
 ;##################################################
 ;########### PROGRAMME PRINCIPAL ##################
 ;##################################################
 
 main:
+
+; appeler la fonction pour générer les coordonnées x1 et y1
+lea rdi, [x1]
+call generer_nombre_aleatoire
+lea rdi, [y1]
+call generer_nombre_aleatoire
+
+; utiliser un modulo pour obtenir des valeurs correctes
+mov cx,65535
+mov ax,[x1]
+div cx
+mov [x1],dx
+mov ax,[y1]
+div cx
+mov [y1],dx
+
+; appeler la fonction pour générer les coordonnées x2 et y2
+lea rdi, [x2]
+call generer_nombre_aleatoire
+lea rdi, [y2]
+call generer_nombre_aleatoire
+
+; utiliser un modulo pour obtenir des valeurs correctes
+mov cx,65535
+mov ax,[x2]
+div cx
+mov [x2],dx
+mov ax,[y2]
+div cx
+mov [y2],dx
+
+; afficher les coordonnées x1, y1 et x2, y2 dans reponse1
+mov rdi,reponse1
+movzx rsi,word[x1]
+movzx rdx,word[y1]
+movzx rcx,word[x2]
+movzx r8,word[y2]
+mov rax,0
+call printf
+
+
+
+
+
+
 xor     rdi,rdi
 call    XOpenDisplay	; Création de display
 mov     qword[display_name],rax	; rax=nom du display
@@ -242,4 +313,3 @@ closeDisplay:
     call    XCloseDisplay
     xor	    rdi,rdi
     call    exit
-	
--- gen_coord_rand.asm
+++ gen_coord_rand.asm
@@ -1,27 +1,72 @@
 extern printf
 
-global main
-
 section .data
-reponse1:   db  "Résultat: %d",10,0
-resultat:	dd	0
+reponse1: db "Coordonnées: (%d,%d), (%d,%d)",10,0
+x1: dd 0
+y1: dd 0
+x2: dd 0
+y2: dd 0
 
 section .bss
-val1:  	resw    1
-
 
 section .text
+
+; la fonction pour générer des nombres aléatoires entiers
+; prend en entrée un pointeur vers un espace de stockage pour le résultat
+; et retourne ce dernier dans EAX
+generer_nombre_aleatoire:
+push rbp
+mov rax,0
+rdrand ax
+jnc lbl_relancer
+mov [rdi],ax
+jmp lbl_fin
+lbl_relancer:
+call generer_nombre_aleatoire
+lbl_fin:
+pop rbp
+ret
+
+global main
 main:
 push rbp
 
-; chosir une valeur aléatoire avec rdrand
-mov rax,0
-rdrand ax
-mov [val1],ax
+; appeler la fonction pour générer les coordonnées x1 et y1
+lea rdi, [x1]
+call generer_nombre_aleatoire
+lea rdi, [y1]
+call generer_nombre_aleatoire
 
-; afficher la valeur aléatoire dans reponse1
+; utiliser un modulo pour obtenir des valeurs correctes
+mov cx,65535
+mov ax,[x1]
+div cx
+mov [x1],dx
+mov ax,[y1]
+div cx
+mov [y1],dx
+
+; appeler la fonction pour générer les coordonnées x2 et y2
+lea rdi, [x2]
+call generer_nombre_aleatoire
+lea rdi, [y2]
+call generer_nombre_aleatoire
+
+; utiliser un modulo pour obtenir des valeurs correctes
+mov cx,65535
+mov ax,[x2]
+div cx
+mov [x2],dx
+mov ax,[y2]
+div cx
+mov [y2],dx
+
+; afficher les coordonnées x1, y1 et x2, y2 dans reponse1
 mov rdi,reponse1
-movzx rsi,word[val1]
+movzx rsi,word[x1]
+movzx rdx,word[y1]
+movzx rcx,word[x2]
+movzx r8,word[y2]
 mov rax,0
 call printf
 
